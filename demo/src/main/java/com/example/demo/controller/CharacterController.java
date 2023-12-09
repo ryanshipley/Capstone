35c0dfb4c2ca4e2b9d91f74aa7f7516623d297a9
ryanshipley <ryanshipley23@gmail.com>
2023-12-09 16:06:50 -0500

    allows for edit functionality

--- demo/src/main/java/com/example/demo/controller/CharacterController.java
+++ demo/src/main/java/com/example/demo/controller/CharacterController.java
@@ -4,7 +4,9 @@ import com.example.demo.model.Character;
 import com.example.demo.repository.CharacterRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,64 +31,70 @@ public class CharacterController {
         return new ModelAndView("create-character");
     }
 
-    @PostMapping("/save")
-    public ModelAndView saveCharacter(
-            @RequestParam String name,
-            @RequestParam String race,
-            @RequestParam String characterClass,
-            @RequestParam String description,
-            @RequestParam String image,
-            @RequestParam int strength,
-            @RequestParam int dexterity,
-            @RequestParam int constitution,
-            @RequestParam int intelligence,
-            @RequestParam int wisdom,
-            @RequestParam int charisma
-    ) {
-        Character character = new Character();
-        character.setName(name);
-        character.setRace(race);
-        character.setCharacterClass(characterClass);
-        character.setDescription(description);
-        character.setImage(image);
-        character.setStrength(strength);
-        character.setDexterity(dexterity);
-        character.setConstitution(constitution);
-        character.setIntelligence(intelligence);
-        character.setWisdom(wisdom);
-        character.setCharisma(charisma);
-
-        // Save the character
+@PostMapping("/save")
+public String saveCharacter(@ModelAttribute Character character) {
+    if (character.getId() == null) {
+        // If the character ID is null, it's a new character, so save it
         characterRepository.save(character);
+    } else {
+        // If the character ID is not null, it's an existing character, so update it
+        Optional<Character> existingCharacter = characterRepository.findById(character.getId());
+        existingCharacter.ifPresent(updatedCharacter -> {
+            // Update the existing character with the new data
+            updatedCharacter.setName(character.getName());
+            updatedCharacter.setRace(character.getRace());
+            updatedCharacter.setCharacterClass(character.getCharacterClass());
+            updatedCharacter.setDescription(character.getDescription());
+            updatedCharacter.setImage(character.getImage());
+            updatedCharacter.setStrength(character.getStrength());
+            updatedCharacter.setDexterity(character.getDexterity());
+            updatedCharacter.setConstitution(character.getConstitution());
+            updatedCharacter.setIntelligence(character.getIntelligence());
+            updatedCharacter.setWisdom(character.getWisdom());
+            updatedCharacter.setCharisma(character.getCharisma());
+            
 
-        ModelAndView modelAndView = new ModelAndView("redirect:/");
-        modelAndView.addObject("successMessage", "Character added successfully!");
-    
-
-        // Redirect to home page or show a success message
-        return new ModelAndView("redirect:/");
+            // Save the updated character
+            characterRepository.save(updatedCharacter);
+        });
     }
 
+    // Redirect to the list of all characters after saving/updating
+    return "redirect:/characters/all";
+}
+
     @GetMapping("/all")
-    public ModelAndView showAllCharacters() {
+    public String getAllCharacters(Model model) {
         List<Character> characters = characterRepository.findAll();
-        ModelAndView modelAndView = new ModelAndView("all-characters");
-        modelAndView.addObject("characters", characters);
-        return modelAndView;
+        model.addAttribute("characters", characters);
+        return "all-characters";
     }
 
     @GetMapping("/details/{id}")
-    public ModelAndView viewCharacterDetails(@PathVariable Long id) {
+    public String viewCharacterDetails(@PathVariable Long id, Model model) {
         Optional<Character> optionalCharacter = characterRepository.findById(id);
 
         if (optionalCharacter.isPresent()) {
             Character character = optionalCharacter.get();
-            ModelAndView modelAndView = new ModelAndView("character-details");
-            modelAndView.addObject("character", character);
-            return modelAndView;
+            model.addAttribute("character", character);
+            return "character-details";
         } else {
-            // Handle case when character with the given id is not found
-            return new ModelAndView("redirect:/characters/all");
+            return "redirect:/characters/all";
         }
     }
+
+    @GetMapping("/edit/{id}")
+    public String editCharacter(@PathVariable Long id, Model model) {
+        Optional<Character> optionalCharacter = characterRepository.findById(id);
+
+        if (optionalCharacter.isPresent()) {
+            Character character = optionalCharacter.get();
+            model.addAttribute("character", character);
+            return "edit-character";
+        } else {
+            return "redirect:/characters/all";
+        }
+    }
+
+
 }
